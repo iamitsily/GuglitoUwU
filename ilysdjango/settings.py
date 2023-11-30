1e262b2d5eb205843cc5af74bb27aeb871f4203f
iamitsily <ilystx1@gmail.com>
2023-11-30 00:34:59 -0600

    a

--- ilysdjango/settings.py
+++ ilysdjango/settings.py
@@ -25,11 +25,14 @@ SECRET_KEY = 'django-insecure-k@lkx!ww_#o$4+&&#$#j58+#abxthr#-1m-_vsp$i6+9xz6ym4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['guglitouwu.azurewebsites.net','http://127.0.0.1:8000/']
-
+ALLOWED_HOSTS = []
+#'guglitouwu.azurewebsites.net','http://127.0.0.1:8000/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'ilysdjangoApp', 'templates', 'Guglitouwu', 'static')
+    os.path.join(BASE_DIR, 'static'),
     ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ilysdjangoApp'
+    'ilysdjangoApp',
+    'ilysdjango'
 ]
 
 MIDDLEWARE = [
@@ -108,19 +112,19 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Mexico_City'  # Cambia a la zona horaria de tu elección
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- ilysdjango/urls.py
+++ ilysdjango/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from ilysdjangoApp import views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path("", include("ilysdjangoApp.urls")),
